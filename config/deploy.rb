e137917b41c3aa0aeeb850d96b0e51bb0789ea24
Richard Huang <flyerhzm@gmail.com>
2010-02-14 22:53:55 +0800

    do not deploy to job

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,2 +1,31 @@
-set :default_stage, "production"
-set :stages, %w(production job)
+set :application, "rubyslide.com"
+set :repository,  "huangzhi@huangzhimin.com:gits/rubyslide.git"
+set :user, "huangzhi"
+set :scm, :git
+set :deploy_to, "/home7/huangzhi/sites/rubyslide.com"
+set :deploy_via, :export
+set(:releases) { capture("ls -x #{releases_path}").split }
+
+role :app, "rubyslide.com"
+role :web, "rubyslide.com"
+role :db,  "rubyslide.com", :primary => true
+
+set :use_sudo, false
+set :run_method, :run
+
+namespace(:deploy) do
+  task :after_update_code, :roles => :app do
+    run "ln -s #{shared_path}/config/database.yml #{current_release}/config/database.yml"
+    run "ln #{shared_path}/.htaccess #{current_release}/public/.htaccess"
+    run "cp #{shared_path}/config/environment.rb #{current_release}/config/"
+    run "ln #{shared_path}/config/initializers/slideshare.rb #{current_release}/config/initializers/slideshare.rb"
+    run "chmod -R u+rwX,go-w #{current_release}/public #{current_release}/log"
+  end
+
+  task :restart do
+    migrate
+    cleanup
+    run "#{current_release}/script/process/reaper --action=restart --dispatcher=dispatch.fcgi"
+  end
+end
+
