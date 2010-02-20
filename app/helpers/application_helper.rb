b4355062060a9c05b70de5120d68fb74cba4ef22
Richard Huang <flyerhzm@gmail.com>
2010-02-20 22:08:23 +0800

    seo for title and description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def tag_name
+    if params[:tag_id]
+      params[:tag_id]
+    end
+  end
 end
