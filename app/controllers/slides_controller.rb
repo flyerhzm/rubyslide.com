9f21b3cb95973dc42152b5944bf4009a2f9e3ede
Richard Huang <flyerhzm@gmail.com>
2010-06-02 22:47:59 +0800

    user's slides

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -3,6 +3,9 @@ class SlidesController < ApplicationController
     if params[:tag_id]
       @slides = Slide.tagged_with(params[:tag_id]).paginate(:page => params[:page], :order => 'slides.created_at DESC')
       @count = Slide.tagged_with(params[:tag_id]).count
+    elsif params[:username]
+      @slides = Slide.find_all_by_username(params[:username]).paginate(:page => params[:page], :order => 'slides.created_at DESC')
+      @count = Slide.find_all_by_username(params[:username]).count
     elsif params[:search_box]
       @slides = Slide.all(:conditions => ["title LIKE ?", "%#{params[:search_box]}%"]).paginate(:page => params[:page], :order => 'slides.created_at DESC')
       @count = Slide.count(:conditions => ["title LIKE ?", "%#{params[:search_box]}%"])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'slides', :action => 'index'
   map.feed '/feed', :controller => 'slides', :action => 'feed'
+  map.user_slides '/user/:username/slides', :controller => 'slides', :action => 'index'
 
   map.resources :tags do |tag|
     tag.resources :slides
