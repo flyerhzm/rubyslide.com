cde3035faf84664e689c2013d9adfdeff1fe8bfa
Richard Huang <flyerhzm@gmail.com>
2011-04-09 17:40:11 +0800

    update cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,18 +6,26 @@ set :deploy_to, "/home/huangzhi/sites/rubyslide.com/production"
 set :deploy_via, :remote_cache
 set :rails_env, :production
 
-role :app, "rubyslide.com"
+require 'bundler/capistrano'
+
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'rvm/capistrano'
+set :rvm_ruby_string, 'ruby-1.9.2-p180@rubyslide.com'
+set :rvm_type, :user
+
+set :whenever_command, "bundle exec whenever"
+require "whenever/capistrano"
+
 role :web, "rubyslide.com"
+role :app, "rubyslide.com"
 role :db,  "rubyslide.com", :primary => true
 
 after "deploy:update_code", "config:init"
 
 namespace :config do
   task :init do
-    run "ln -s #{shared_path}/config/database.yml #{current_release}/config/database.yml"
-    run "cp #{shared_path}/config/environment.rb #{current_release}/config/"
-    run "ln #{shared_path}/config/initializers/slideshare.rb #{current_release}/config/initializers/slideshare.rb"
-    run "chmod -R u+rwX,go-w #{current_release}/public #{current_release}/log"
+    run "ln -nfs #{shared_path}/config/database.yml #{current_release}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/initializers/slideshare.rb #{current_release}/config/initializers/slideshare.rb"
   end
 end
 
