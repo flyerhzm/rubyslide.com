70d9789278e33363711e0a51435e0810d1d47ef6
Richard Huang <flyerhzm@gmail.com>
2011-10-23 18:10:19 +0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
-ActionController::Routing::Routes.draw do |map|
-  map.root :controller => 'slides', :action => 'index'
-  map.feed '/feed', :controller => 'slides', :action => 'feed'
-  map.user_slides '/user/:username/slides', :controller => 'slides', :action => 'index'
-  map.user_feed '/user/:username/feed', :controller => 'slides', :action => 'feed'
-
-  map.resources :tags do |tag|
-    tag.resources :slides
+Rubyslide::Application.routes.draw do
+  match '/' => 'slides#index'
+  match '/feed' => 'slides#feed', :as => :feed
+  match '/user/:username/slides' => 'slides#index', :as => :user_slides
+  match '/user/:username/feed' => 'slides#feed', :as => :user_feed
+  resources :tags do
+    resources :slides
   end
 end
