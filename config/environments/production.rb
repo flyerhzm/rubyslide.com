04346b84626da76a5165e20ff450b1d24ec593e8
Richard Huang <flyerhzm@gmail.com>
2011-10-23 20:48:50 +0800

    enable memcache and asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,14 +25,14 @@ Rubyslide::Application.configure do
   # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
-  # config.cache_store = :mem_cache_store
+  config.cache_store = ActiveSupport::Cache::MemCacheStore.new(Memcached::Rails.new("localhost:11211", :namespace => "rubyslide", :logger => Rails.logger))
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
   config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "http://assets.rubyslide.com"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
